b79603b9c44b14197ba7278958f819477e6dd9e1
dkrasot <ik5209101>
2015-11-08 23:23:38 +0200

    1st one

---
+++ SpringREST/src/main/java/hello/GreetingController.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+//methods return domain objects instead of views ( like @Controller + @ResponseBody )
+// http://localhost:8080/greeting -> Response with JSON {"id":1,"content":"Hello, World!"}
+// http://localhost:8080/greeting?name=Ukraine -> {"id":2,"content":"Hello, Ukraine!"}
+public class GreetingController {
+
+    private static final String temlate = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(temlate, name));
+        //this object will be converted to JSON automatically because Jackson2 lib in classpath
+    }
+
+}
