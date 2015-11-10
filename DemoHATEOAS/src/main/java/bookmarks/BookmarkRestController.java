21722769b290d6efb2fe8373dfe2a75f0aa8ea0a
dkrasot <dkrasota@gmail.com>
2015-11-10 16:45:51 +0200

    adding error handling and Resource

--- DemoHATEOAS/src/main/java/bookmarks/BookmarkRestController.java
+++ DemoHATEOAS/src/main/java/bookmarks/BookmarkRestController.java
@@ -1,13 +1,18 @@
 package bookmarks;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/{userId}/bookmarks")
@@ -27,29 +32,35 @@ public class BookmarkRestController {
         this.validateUser(userId);
         return this.accountRepository.findByUsername(userId)
                 .map( account -> {
-                    Bookmark result = bookmarkRepository.save(
+                    Bookmark bookmark = bookmarkRepository.save(
                             new Bookmark(account, input.uri, input.description));
                     HttpHeaders httpHeaders = new HttpHeaders();
-                    httpHeaders.setLocation(
-                            ServletUriComponentsBuilder
-                                    .fromCurrentRequest().path("/{id}")
-                                    .buildAndExpand(result.getId()).toUri());
+
+                    Link forOneBookmark = new BookmarkResource(bookmark).getLink("self");
+                    httpHeaders.setLocation(URI.create(forOneBookmark.getHref()));
+//                    httpHeaders.setLocation(
+//                            ServletUriComponentsBuilder
+//                                    .fromCurrentRequest().path("/{id}")
+//                                    .buildAndExpand(bookmark.getId()).toUri());
                     return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
                 }).get();
     }
 
     @RequestMapping(value = "/{bookmarkId}", method = RequestMethod.GET)
-    Bookmark readBookmark(@PathVariable String userId, @PathVariable Long bookmarkId){
+    BookmarkResource readBookmark(@PathVariable String userId, @PathVariable Long bookmarkId){
         this.validateUser(userId);
-        return this.bookmarkRepository.findOne(bookmarkId);
+        //return new this.bookmarkRepository.findOne(bookmarkId); --> Bookmark
+        return new BookmarkResource(this.bookmarkRepository.findOne(bookmarkId));
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    Collection<Bookmark> readBookmarks(@PathVariable String userId){
+    Resources<BookmarkResource> readBookmarks(@PathVariable String userId){
         this.validateUser(userId);
-        return this.bookmarkRepository.findByAccountUsername(userId);
+        List<BookmarkResource> bookmarkResourceList = bookmarkRepository.findByAccountUsername(userId)
+                .stream().map(BookmarkResource::new).collect(Collectors.toList());
+        //return this.bookmarkRepository.findByAccountUsername(userId); --> Collection<Bookmark>
+        return new Resources<BookmarkResource>(bookmarkResourceList);
     }
-
 
     private void validateUser(String userId) {
         this.accountRepository.findByUsername(userId).orElseThrow(
