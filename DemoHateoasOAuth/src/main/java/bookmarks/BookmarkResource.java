6b1fa3d2a953476660172a270e855ba951ecc35d
dkrasot <dkrasota@gmail.com>
2015-11-11 16:02:35 +0200

    tmp fix of Link with Principal

--- DemoHateoasOAuth/src/main/java/bookmarks/BookmarkResource.java
+++ DemoHateoasOAuth/src/main/java/bookmarks/BookmarkResource.java
@@ -16,12 +16,11 @@ public class BookmarkResource extends ResourceSupport {
     public BookmarkResource(Bookmark bookmark) {
         this.bookmark = bookmark;
 
-        
         String username = bookmark.getAccount().getUsername();
         this.add(new Link(bookmark.getUri(), "bookmark-uri"));
         this.add(linkTo(BookmarkRestController.class, username).withRel("bookmarks"));
-        this.add(linkTo(methodOn(BookmarkRestController.class, username)
-                            .readBookmark(username, bookmark.getId())).withSelfRel());
+//        this.add(linkTo(methodOn(BookmarkRestController.class, username)
+//                            .readBookmark(username, bookmark.getId())).withSelfRel());
     }
 
     public Bookmark getBookmark() {
